b832647f7d4e709a18dfc2917f0fb4d8d28864ef
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2022-09-13 13:04:39 +0200

    Binary Tree

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            binaryTree tree = new binaryTree();
+            tree.Insert();
+            tree.DisplayNodes();
+
+        }
+    }
+
+    public class binaryTree
+    {
+        public class Node
+        {
+            public int Data;
+            public Node? Left;
+            public Node? Right;
+
+            public void writeNode()
+            {
+                Console.WriteLine(Data);
+                Left?.writeNode();
+                Right?.writeNode();
+            }
+        }
+
+        public void DisplayNodes()
+        {
+            Console.WriteLine(root?.Data);
+            root?.Left?.writeNode();
+            root?.Right?.writeNode();
+        }
+
+        public Node? root;
+
+        public void Insert()
+        {
+            Node rootNode = new Node();
+            Node rightChild = new Node();
+            Node leftChild = new Node();
+
+            rootNode.Data = 1;
+            rootNode.Left = leftChild;
+            rootNode.Right = rightChild;
+
+            leftChild.Data = 5;
+            rightChild.Data = 20;
+
+            root = rootNode;
+        }
+    }
+}
