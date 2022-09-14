99c26efcf713463be068878dba23fcd5dc45ccb5
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2022-09-14 17:37:27 +0200

    Added a function to invert the binary tree.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace BinaryTree
             tree.Insert();
             tree.DisplayNodes();
 
+            //Inverts the binary tree
+            //          tree.InvertTree(treeRoot: tree.root!);
+            //          tree.DisplayNodes();
         }
     }
 
@@ -44,14 +47,43 @@ namespace BinaryTree
             Node rightChild = new Node();
             Node leftChild = new Node();
 
+            Node leftChildLeft = new Node();
+            leftChildLeft.Data = 22;
+            Node leftChildRight = new Node();
+            leftChildRight.Data = 51;
+            Node rightChildLeft = new Node();
+            rightChildLeft.Data = 99;
+            Node rightChildRight = new Node();
+            rightChildRight.Data = 122;
+
             rootNode.Data = 1;
             rootNode.Left = leftChild;
             rootNode.Right = rightChild;
+            leftChild.Left = leftChildLeft;
+            leftChild.Right = leftChildRight;
+            rightChild.Left = rightChildLeft;
+            rightChild.Right = rightChildRight;
 
             leftChild.Data = 5;
             rightChild.Data = 20;
 
             root = rootNode;
+        }
+
+        public Node InvertTree(Node treeRoot)
+        {
+            if (treeRoot == null)
+            {
+                return treeRoot!;
+            }
+
+            Node left = InvertTree(treeRoot: treeRoot.Left!);
+            Node right = InvertTree(treeRoot: treeRoot.Right!);
+
+            treeRoot.Left = right;
+            treeRoot.Right = left;
+
+            return treeRoot;
         }
     }
 }
